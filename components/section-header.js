695d764fd589fa2532f1faae5863b055c1f6b10c
Dan <dan@ariesliving.com>
2020-09-14 11:58:43 -0400

    style modules

--- components/section-header.js
+++ components/section-header.js
@@ -1,32 +1,11 @@
 import Container from './container'
+import cn from 'classnames'
+import styles from './section-header.module.css'
 
-export default function SectionHeader({
-  id,
-  title,
-  description,
-  className,
-  margin = '0 0 2rem 0'
-}) {
+export default function SectionHeader({ id, title, description, className }) {
   return (
     <Container padding wide gray>
-      <div className={className} id={id}>
-        <style jsx>
-          {`
-            div {
-              position: relative;
-              text-align: center;
-              margin: ${margin};
-            }
-            h2 {
-              letter-spacing: -1px;
-            }
-            @media screen and (max-width: 640px) {
-              div {
-                padding: 0 var(--gap);
-              }
-            }
-          `}
-        </style>
+      <div className={cn(styles['main'], className)} id={id}>
         <h2 className="fs fw3">{title}</h2>
         {description && <p className="f-reset fp fw4">{description}</p>}
       </div>
--- components/section.js
+++ components/section.js
@@ -1,39 +1,12 @@
 import Image from './image'
+import styles from './section.module.css'
+import cn from 'classnames'
 
 export default function Section({ description, id, reverse, title, alt, src, width, height }) {
   return (
-    <div className="main" id={id}>
-      <style jsx>
-        {`
-          .main {
-            position: relative;
-            display: flex;
-            flex-direction: ${reverse ? 'row-reverse' : 'row'};
-            margin: var(--gap) 0;
-          }
-          .content {
-            margin: ${reverse ? `0 var(--gap-triple) 0 0` : `0 0 0 var(--gap-triple)`};
-            display: flex;
-            flex-direction: column;
-            justify-content: center;
-          }
-          .content > h2 {
-            margin-bottom: var(--gap-double);
-          }
-          @media screen and (max-width: 960px) {
-            .main {
-              flex-direction: column;
-              text-align: center;
-            }
-            .content {
-              padding: 0 var(--gap);
-              margin: 0;
-            }
-          }
-        `}
-      </style>
+    <div className={cn(styles.main, { [styles.reverse]: reverse })} id={id}>
       <Image className="no-drag" shadow alt={alt} src={src} width={width} height={height} />
-      <div className="content">
+      <div className={styles.content}>
         <h2 className="fs fw4">{title}</h2>
         <p className="f-reset fp fw4">{description}</p>
       </div>
