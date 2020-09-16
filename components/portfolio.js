66e880d0e3adedaf0a809e96c5a338dfa174c014
Dan <dan@ariesliving.com>
2020-09-16 10:05:49 -0400

    mobile styles

--- components/portfolio.js
+++ components/portfolio.js
@@ -6,25 +6,38 @@ import styles from './portfolio.module.css'
 
 export default function Portfolio({ description, id, title, width, height }) {
   return (
-    <Container wide role="region" aria-labelledby="portfolio">
+    <Container center padding wide role="region" aria-labelledby="portfolio">
       <div className={styles['main']} id={id}>
         <h2 className="lines fp fw4">{title}</h2>
-        <Container wide center>
-          <p className="f-reset fs f4 fw3">{description}</p>
+        <p className="fs f4 fw3">{description}</p>
+        <Container padding wide>
           <div className={styles['images']}>
             {HOTELS.map(({ src, alt, name, url }) => (
               <div className={styles['card']} key={name}>
                 <a href={url} target="_blank" rel="noopener noreferrer">
                   <p className={cn('fp fw4', alt)}>{name}</p>
-                  <Image
-                    shadow
-                    margin={60}
-                    layout="responsive"
-                    alt={alt}
-                    src={src}
-                    width={width}
-                    height={height}
-                  />
+                  <div className="hide-tablet">
+                    <Image
+                      shadow
+                      margin={20}
+                      layout="responsive"
+                      alt={alt}
+                      src={src}
+                      width={width}
+                      height={height}
+                    />
+                  </div>
+                  <div className="display-tablet">
+                    <Image
+                      shadow
+                      margin={10}
+                      layout="responsive"
+                      alt={alt}
+                      src={src}
+                      width={width * 1.3}
+                      height={height * 1.3}
+                    />
+                  </div>
                 </a>
               </div>
             ))}
