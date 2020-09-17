100436d982d110c1300ce1ea42c8f1ab2bd26f88
Dan <dan@ariesliving.com>
2020-09-17 11:21:27 -0400

    change image randomization

--- components/lines-gallery.js
+++ components/lines-gallery.js
@@ -3,38 +3,26 @@ import Lines from './icons/lines'
 import Image from './image'
 import styles from './lines-gallery.module.css'
 import cn from 'classnames'
-
-const images = [
-  { src: '/showcase-thumbnails/ithaca/rooms/cks/5.jpg', alt: 'Ithaca Classic King Suite' },
-  {
-    src: '/showcase-thumbnails/villa-paradiso/rooms/multi-unit/1.jpg',
-    alt: 'Villa Paradiso Suite'
-  },
-  { src: '/showcase-thumbnails/ithaca/rooms/sk/2.jpg', alt: 'Ithaca Superior King' },
-  { src: '/showcase-thumbnails/treehouse/rooms/ql/1.jpg', alt: 'Treehouse Queen Loft' },
-  { src: '/showcase-thumbnails/villa-paradiso/rooms/16/1.jpg', alt: 'Villa Paradiso Multi Unit' }
-]
+import { shuffled } from 'lib/constants'
 
 const LinesGallery = () => (
   <Container dotBackground wide role="region" aria-labelledby="lines-gallery">
     <div className={cn(styles['container'], 'hide-tablet')}>
       <div className={styles['images']}>
-        {images
-          .sort(() => Math.random() - 0.5)
-          .map(({ src, alt }, i) => (
-            <div key={i}>
-              <Image
-                shadow
-                oversize={false}
-                width={282}
-                height={166}
-                src={src}
-                alt={alt}
-                layout="responsive"
-                margin={0}
-              />
-            </div>
-          ))}
+        {shuffled.map(({ src, alt }) => (
+          <div key={alt}>
+            <Image
+              shadow
+              oversize={false}
+              width={282}
+              height={166}
+              src={`/showcase-thumbnails/${src}`}
+              alt={alt}
+              layout="responsive"
+              margin={0}
+            />
+          </div>
+        ))}
       </div>
 
       <div className={styles['lines-container']}>
