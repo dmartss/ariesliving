89212dd4959d9b26611147e833f129cae9cb9f10
Dan <dmartss13@gmail.com>
2020-10-09 11:07:32 -0400

    gradient styles

--- components/newsletter/index.js
+++ components/newsletter/index.js
@@ -1,5 +1,6 @@
-import Container from '../container'
-import EmailForm from '../email-form'
+import cn from 'classnames'
+import Container from 'components/container'
+import EmailForm from 'components/email-form'
 import styles from './newsletter.module.css'
 
 export default function Newsletter() {
@@ -7,8 +8,16 @@ export default function Newsletter() {
     <Container wide overflow center padding role="region" aria-labelledby="customers">
       <div className={styles.content}>
         <aside>
-          <h2 className="fp">DON'T MISS OUT</h2>
-          <h3 className="fs">Stay up to date with Aries Living</h3>
+          <h2 aria-label="DON'T MISS OUT">
+            <span className={cn(styles['gradient-bg'], styles['gradient-bg-1'])}>
+              <span className={styles['gradient-fg']}>DON'T MISS OUT</span>
+            </span>
+          </h2>
+          <h3 aria-label="Stay up to date with Aries Living">
+            <span className={cn(styles['gradient-bg'], styles['gradient-bg-2'])}>
+              <span className={styles['gradient-fg']}>Stay up to date with Aries Living</span>
+            </span>
+          </h3>
         </aside>
         <aside>
           <div className={styles['email-form']}>
