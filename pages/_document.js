e46e88f926388e1608ff2ffa1aac25be41fcda6c
Dan <dmartss13@gmail.com>
2020-10-08 21:19:45 -0400

    team flex

--- pages/_document.js
+++ pages/_document.js
@@ -11,11 +11,11 @@ class AriesLiving extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
           <link rel="manifest" href="/favicon/site.webmanifest" />
-          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
+          <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000" />
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
-          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileColor" content="#000" />
           <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" />
+          <meta name="theme-color" content="#000" />
         </Head>
         <body>
           <Main />
