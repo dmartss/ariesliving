5b66f9c82fddf9f9bc48c13657626c4b8de5223c
Dan <dmartss13@gmail.com>
2020-10-16 09:51:19 -0400

    hotel with skeleton

--- pages/hotels/[hotel].js
+++ pages/hotels/[hotel].js
@@ -2,9 +2,12 @@ import { SkipNavContent } from '@reach/skip-nav'
 import Error from 'next/error'
 import { useRouter } from 'next/router'
 import FeedbackContext from 'components/feedback/feedback-context'
-import Image from 'components/image'
+import Skeleton from 'components/skeleton'
+import Container from 'components/container'
+import Hotel from 'components/hotel'
 import Page from 'components/page'
 import hotels from 'hotels'
+import styles from 'styles/pages/hotel.module.css'
 
 export const getStaticPaths = () => ({
   paths: hotels.map(({ hotel }) => ({ params: { hotel } })),
@@ -14,7 +17,7 @@ export const getStaticProps = ({ params }) => {
   return { props: { hotel: { ...hotels.find(e => e.hotel === params.hotel) } } }
 }
 
-export default function Hotel({ hotel }) {
+export default function HotelPage({ hotel }) {
   const router = useRouter()
   const { asPath, isFallback } = router
 
@@ -32,17 +35,15 @@ export default function Hotel({ hotel }) {
           url={asPath}
         >
           <SkipNavContent />
-          <Image
-            src={hotel.src}
-            shadow
-            margin={0}
-            noBorder
-            oversize={false}
-            layout="responsive"
-            width={3000 / 2.75}
-            height={2000 / 2.75}
-            alt={`${hotel.title} Image`}
-          />
+          {hotel ? (
+            <Hotel hotel={hotel} />
+          ) : (
+            <Container>
+              <div className={styles.container}>
+                <Skeleton style={{ height: 'calc(100% - 5.5rem)' }} />
+              </div>
+            </Container>
+          )}
         </Page>
       </div>
     </FeedbackContext.Provider>
