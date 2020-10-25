a4de8b9b8025b88df04ab72fd03ea3346806973c
Dan <dmartss13@gmail.com>
2020-10-25 09:19:34 -0400

    reorganize collapse context

--- components/collapse/collapse-context.js
+++
@@ -1,6 +0,0 @@
-import React, { useContext } from 'react'
-
-const CollapseContext = React.createContext()
-export const Provider = CollapseContext.Provider
-export const Consumer = CollapseContext.Consumer
-export const useCollapse = () => useContext(CollapseContext)
--- components/collapse/index.js
+++ components/collapse/index.js
@@ -1,2 +1,120 @@
-export { default, default as Collapse } from './collapse'
-export { default as CollapseGroup } from './collapse-group'
+import { useState, useCallback, useMemo, useRef, useEffect } from 'react'
+import { useSpring, animated } from 'react-spring'
+import useMeasure from 'react-use-measure'
+import { ResizeObserver } from '@juggle/resize-observer'
+import cn from 'classnames'
+import { Provider, useCollapse } from 'lib/collapse-context'
+import { useIsMobile, useIsTablet } from 'lib/media-query'
+import { ChevronDown } from 'components/icons'
+import styles from './collapse.module.css'
+
+export function CollapseGroup({ children }) {
+  const [selected, setSelected] = useState('')
+  const onChange = title => {
+    setSelected(title)
+  }
+
+  return (
+    <div className={styles['collapse-group']}>
+      <Provider value={{ selected, onChange }}>{children}</Provider>
+    </div>
+  )
+}
+
+const Collapse = ({ title, subtitle, id, onToggle, card, size, children }) => {
+  const [ref, { height }] = useMeasure({ polyfill: ResizeObserver })
+  const [active, setActive] = useState(false)
+  const collapseContext = useCollapse()
+  const immediateAnimation = useRef(false)
+
+  useIsMobile()
+  useIsTablet()
+
+  const toggle = useCallback(() => setActive(!active), [active])
+
+  const open = useMemo(() => {
+    return collapseContext
+      ? !collapseContext.selected
+        ? active
+        : collapseContext.selected === title
+      : active
+  }, [collapseContext, active, title])
+
+  const _toggle = useMemo(() => {
+    return collapseContext
+      ? () => {
+          collapseContext.onChange(open ? null : title)
+          if (typeof onToggle === 'function') onToggle()
+        }
+      : toggle
+  }, [collapseContext, open, title, onToggle, toggle])
+
+  const onKeyPress = useCallback((e, cb) => {
+    if (e.key === ' ' || e.key === 'Enter') {
+      cb()
+      e.preventDefault()
+    }
+  }, [])
+
+  const props = useSpring({
+    height: open ? height : 0,
+    immediate: immediateAnimation.current,
+    config: { tension: 250, friction: 32, clamp: true }
+  })
+
+  useEffect(() => {
+    let timeout
+    const hash = location.hash.slice(1)
+    if (id === hash) {
+      immediateAnimation.current = true
+      if (collapseContext) {
+        collapseContext.onChange(title)
+      } else {
+        setActive(true)
+      }
+      timeout = setTimeout(() => {
+        immediateAnimation.current = false
+      }, 1000)
+    }
+
+    return () => {
+      if (timeout) clearTimeout(timeout)
+    }
+  }, [])
+
+  return (
+    <div
+      className={cn('tl', styles.collapse, {
+        [styles.card]: card,
+        [styles.small]: size === 'small',
+        [styles.border]: !collapseContext
+      })}
+      id={id}
+    >
+      <div
+        role="button"
+        tabIndex="0"
+        className={styles['collapse-title']}
+        aria-expanded={open}
+        onClick={_toggle}
+        onKeyPress={ev => onKeyPress(ev, _toggle)}
+      >
+        <div className={styles.top}>
+          <h3 className="fw5">{title}</h3>
+          <span className={cn(styles.icon, { [styles.open]: open })}>
+            <ChevronDown />
+          </span>
+        </div>
+
+        {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
+      </div>
+      <animated.div style={{ overflow: 'hidden', ...props }}>
+        <div ref={ref} className={cn(styles['collapse-content'], { [styles.open]: open })}>
+          {children}
+        </div>
+      </animated.div>
+    </div>
+  )
+}
+
+export default Collapse
