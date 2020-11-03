835725930969d861662e4aecbe4f40dacfa90156
Dan <dmartss13@gmail.com>
2020-11-03 09:07:39 -0500

    use on scroll back to original

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import nprogress from 'nprogress'
 import Router from 'next/router'
 import { trackPageview } from 'lib/analytics'
 import { Provider } from 'lib/nav-context'
-import { ScrollContext } from 'lib/scroll-context'
 import { ThemeProvider } from 'next-themes'
 import 'styles/global.css'
 
@@ -11,27 +10,7 @@ let timeout
 
 export default function App({ Component, pageProps }) {
   const [mobileNavShown, setMobileNavShown] = useState(false)
-  const [scrollPosition, setScrollPosition] = useState(
-    (typeof window !== 'undefined' && window.pageYOffset) || 0
-  )
-
   const toggle = () => setMobileNavShown(!mobileNavShown)
-
-  const onScroll = () => {
-    requestAnimationFrame(() => {
-      setScrollPosition(window.pageYOffset)
-    })
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', onScroll)
-    return () => {
-      window.removeEventListener('scroll', onScroll)
-    }
-  }, [])
-
-  const headerLock = scrollPosition >= 73
-  const secondHeaderLock = scrollPosition <= 865
 
   const start = url => {
     timeout = setTimeout(nprogress.start, 200)
@@ -43,7 +22,6 @@ export default function App({ Component, pageProps }) {
     setMobileNavShown(false)
     nprogress.done()
   }
-
   useEffect(() => {
     Router.events.on('routeChangeStart', start)
     Router.events.on('routeChangeComplete', done)
@@ -60,11 +38,9 @@ export default function App({ Component, pageProps }) {
 
   return (
     <ThemeProvider disableTransitionOnChange defaultTheme="dark">
-      <ScrollContext.Provider value={{ headerLock, secondHeaderLock }}>
-        <Provider value={{ mobileNavShown, toggle }}>
-          <Component {...pageProps} />
-        </Provider>
-      </ScrollContext.Provider>
+      <Provider value={{ mobileNavShown, toggle }}>
+        <Component {...pageProps} />
+      </Provider>
     </ThemeProvider>
   )
 }
