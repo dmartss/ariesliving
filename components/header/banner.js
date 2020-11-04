38e70b6209cbd3a8a129136abeb4c785ec4ab481
Dan <dmartss13@gmail.com>
2020-11-04 07:15:14 -0500

    remove container from banner

--- components/header/banner.js
+++ components/header/banner.js
@@ -1,4 +1,3 @@
-import Container from 'components/container'
 import useSubscribe from 'lib/use-subscribe'
 import EmailForm from 'components/email-form'
 import s from './banner.module.css'
@@ -6,17 +5,15 @@ import s from './banner.module.css'
 export default function Banner() {
   const { onEmail, loading, success, errorMessage } = useSubscribe()
   return (
-    <Container wide>
-      <div className={s.banner}>
-        {success ? (
-          <p>Thanks for subscribing! Please check your email shortly to confirm</p>
-        ) : (
-          <div className={s.inner}>
-            <p>Stay up to date with Aries Living</p>
-            <EmailForm errorMessage={errorMessage} loading={loading} onEmail={onEmail} flex />
-          </div>
-        )}
-      </div>
-    </Container>
+    <div className={s.banner}>
+      {success ? (
+        <p>Thanks for subscribing! Please check your email shortly to confirm</p>
+      ) : (
+        <div className={s.inner}>
+          <p>Stay up to date with Aries Living</p>
+          <EmailForm errorMessage={errorMessage} loading={loading} onEmail={onEmail} flex />
+        </div>
+      )}
+    </div>
   )
 }
